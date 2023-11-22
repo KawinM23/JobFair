a83dd46c3486a44c59ca8b4a95b21d8902d49904
KawinM23 <kawin.m235@gmail.com>
2023-11-22 16:10:40 +0700

    Fix page height

--- JobFairFrontend/src/app/user/login/page.tsx
+++ JobFairFrontend/src/app/user/login/page.tsx
@@ -5,7 +5,7 @@ export default function page(props: {
   searchParams?: Record<"callbackUrl" | "error", string>;
 }) {
   return (
-    <main className="center-container h-screen p-5 flex-col">
+    <main className="center-container h-[calc(100vh-5rem)] p-5 flex-col">
       <h1 className="text-2xl font-bold mb-5">Login</h1>
       <LoginForm searchParams={props.searchParams} />
       <Link href={"/user/register"} sx={{ marginTop: 3 }}>
